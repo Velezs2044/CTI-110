1bf7f7379483b879542f1e5cceaa1c0aecd761ea
Velezs2044 <160040592+Velezs2044@users.noreply.github.com>
2024-02-14 10:30:54 -0500

    Add files via upload

---
+++ P1LAB1_VelezSteven.py
@@ -0,0 +1,11 @@
+#Steven Velez
+#2/12.2024
+#P1LAB1
+#Using input and output
+
+#Creating a variable to hold first name
+first_name = input("Enter your first name: ")
+
+#Print first_name with greeting
+print("Hey", first_name, "\n", "welcome to CTI-110")
+
